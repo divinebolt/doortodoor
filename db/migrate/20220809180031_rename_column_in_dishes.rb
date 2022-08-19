3acb20b589ec28829083d5b764dde00b24672f42
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-08-19 14:00:51 +0100

    migrate deleted

--- db/migrate/20220809180031_rename_column_in_dishes.rb
+++
@@ -1,5 +0,0 @@
-class RenameColumnInDishes < ActiveRecord::Migration[6.1]
-  def change
-    rename_column :dishes, :descrition, :description
-  end
-end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_08_09_180031) do
+ActiveRecord::Schema.define(version: 2022_08_08_195258) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -32,7 +32,7 @@ ActiveRecord::Schema.define(version: 2022_08_09_180031) do
 
   create_table "reviews", force: :cascade do |t|
     t.text "comment"
-    t.integer "dish_id", null: false
+    t.bigint "dish_id", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.index ["dish_id"], name: "index_reviews_on_dish_id"
