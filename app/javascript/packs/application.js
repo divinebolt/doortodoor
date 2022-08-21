bee5263c1821697676c9bd8b96a069b11d2444ae
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-08-21 23:37:45 +0100

    haaaaaaa

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,10 +8,18 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-//import 'swiper/css/bundle'
+import 'swiper/css/bundle'
 // In your application.js (for example)
 import 'swiper/swiper-bundle.min.css'
 //require("../custom/custom");
+ // import Swiper JS
+ import Swiper from 'swiper/bundle';
+ // import Swiper styles
+ import 'swiper/css';
+
+
+
+
 
 Rails.start()
 Turbolinks.start()
@@ -19,3 +27,47 @@ ActiveStorage.start()
 
 
 import "controllers"
+
+const swiper = new Swiper('.swiper', {
+  // Default parameters
+  slidesPerView: 1,
+  spaceBetween: 10,
+
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination',
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+  // Responsive breakpoints
+  breakpoints: {
+    // when window width is >= 320px
+    320: {
+      slidesPerView: 2,
+      spaceBetween: 20
+    },
+    // when window width is >= 480px
+    480: {
+      slidesPerView: 3,
+      spaceBetween: 30
+    },
+    // when window width is >= 640px
+    640: {
+      slidesPerView: 4,
+      spaceBetween: 40
+    }
+  }
+  // And if we need scrollbar
+  //scrollbar: {
+  //  el: '.swiper-scrollbar',
+  //},
+});
