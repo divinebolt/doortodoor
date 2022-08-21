7ce9c1bbdde1c6dbf7f4c1e03602ce2e95bbd170
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-08-21 16:27:51 +0100

    navbar altered and index git some cards

--- app/models/dish.rb
+++ app/models/dish.rb
@@ -1,5 +1,6 @@
 class Dish < ApplicationRecord
   has_many :reviews, dependent: :destroy
+  has_one_attached :image
   # Geocode map
   geocoded_by :address
   after_validation :geocode, if: :will_save_change_to_address?
