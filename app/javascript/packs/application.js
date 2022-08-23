033b5b0cab33bf4243089b064e35b44c14457a02
JoÃao Bernardo <joaogoncalob@gmail.com>
2022-08-24 00:35:25 +0100

    gem added

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,14 +8,14 @@ import Turbolinks from "turbolinks"
 import * as ActiveStorage from "@rails/activestorage"
 import "channels"
 import "bootstrap"
-import 'swiper/css/bundle'
+//import 'swiper/css/bundle'
 // In your application.js (for example)
-import 'swiper/swiper-bundle.min.css'
+//import 'swiper/swiper-bundle.min.css'
 //require("../custom/custom");
  // import Swiper JS
- import Swiper from 'swiper/bundle';
+ //import Swiper from 'swiper/bundle';
  // import Swiper styles
- import 'swiper/css';
+ //import 'swiper/css';
 
 
 
@@ -28,46 +28,46 @@ ActiveStorage.start()
 
 import "controllers"
 
-const swiper = new Swiper('.swiper', {
-  // Default parameters
-  slidesPerView: 1,
-  spaceBetween: 10,
+// const swiper = new Swiper('.swiper', {
+//   // Default parameters
+//   slidesPerView: 1,
+//   spaceBetween: 10,
 
-  // Optional parameters
-  direction: 'horizontal',
-  loop: true,
+//   // Optional parameters
+//   direction: 'horizontal',
+//   loop: true,
 
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-  },
+//   // If we need pagination
+//   pagination: {
+//     el: '.swiper-pagination',
+//   },
 
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
+//   // Navigation arrows
+//   navigation: {
+//     nextEl: '.swiper-button-next',
+//     prevEl: '.swiper-button-prev',
+//   },
 
-  // Responsive breakpoints
-  breakpoints: {
-    // when window width is >= 320px
-    320: {
-      slidesPerView: 2,
-      spaceBetween: 20
-    },
-    // when window width is >= 480px
-    480: {
-      slidesPerView: 3,
-      spaceBetween: 30
-    },
-    // when window width is >= 640px
-    640: {
-      slidesPerView: 4,
-      spaceBetween: 40
-    }
-  }
-  // And if we need scrollbar
-  //scrollbar: {
-  //  el: '.swiper-scrollbar',
-  //},
-});
+//   // Responsive breakpoints
+//   breakpoints: {
+//     // when window width is >= 320px
+//     320: {
+//       slidesPerView: 2,
+//       spaceBetween: 20
+//     },
+//     // when window width is >= 480px
+//     480: {
+//       slidesPerView: 3,
+//       spaceBetween: 30
+//     },
+//     // when window width is >= 640px
+//     640: {
+//       slidesPerView: 4,
+//       spaceBetween: 40
+//     }
+//   }
+//   // And if we need scrollbar
+//   //scrollbar: {
+//   //  el: '.swiper-scrollbar',
+//   //},
+// });
